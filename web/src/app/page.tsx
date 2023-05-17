42fa4fd220122302fc67f81f651d2b92555b9680
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-16 21:15:42 -0300

    feat(web): setup fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="h-screen bg-zinc-950">oi</div>
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  )
 }
