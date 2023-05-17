f9683f428d0919f307b0ba04e613284fffe9f3ec
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-17 15:27:33 -0300

    feat(web): create hero and copyright elements

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,17 +1,20 @@
+import Image from 'next/image'
 import { User } from 'lucide-react'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      {/* left */}
+      {/* LEFT */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        {/* blur */}
+        {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
-        {/* stripes */}
+        {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        {/* signin section */}
+        {/* Sign In */}
         <a
           href=""
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
@@ -25,9 +28,42 @@ export default function Home() {
             memórias!
           </p>
         </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Selecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com {'<3'} no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
-      {/* right */}
+      {/* RIGHT */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
